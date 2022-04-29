fe42cda9fa4ab8cc5e7fdfce49095a3b87116376
Jensen Ruud <jensen.is.testing@gmail.com>
2022-04-29 17:09:04 -0500

    Fixed all four issues

--- src/ui.js
+++ src/ui.js
@@ -1,11 +1,11 @@
-import {fetchCompanies} from "./api";
+import { fetchCompanies } from "./api";
 import {
   ACCOUNT_EXECUTIVE_FIELD_NAME,
   COMPANIES_TABLE_HEADERS,
   COMPANY_NAME_FIELD_NAME,
   CREATED_AT_FIELD_NAME,
   REVENUE_YTD_FIELD_NAME,
-  STATUS_FIELD_NAME
+  STATUS_FIELD_NAME,
 } from "./constants";
 
 export const makeTable = async () => {
@@ -19,8 +19,18 @@ export const makeTable = async () => {
   const companiesToDisplay = [];
   companiesToDisplay.push(COMPANIES_TABLE_HEADERS);
 
+  // Convert formats of the date and revenue fields
+  companies.map((company) => {
+    company[CREATED_AT_FIELD_NAME] = convertTime(
+      company[CREATED_AT_FIELD_NAME]
+    );
+    company[REVENUE_YTD_FIELD_NAME] = convertRevenue(
+      company[REVENUE_YTD_FIELD_NAME]
+    );
+  });
+
   // Here we simply rearrange company fields in the order in which we want to display them in UI
-  companies.map(company => {
+  companies.map((company) => {
     const row = [];
     row.push(
       company[COMPANY_NAME_FIELD_NAME],
@@ -36,12 +46,33 @@ export const makeTable = async () => {
   const table = document.createElement("table");
   document.body.appendChild(table); // Drew the main table node on the document
 
-  companiesToDisplay.forEach(row => {
+  companiesToDisplay.forEach((row) => {
     const tr = table.insertRow(); //Create a new row
 
-    row.forEach(column => {
+    row.forEach((column) => {
       const td = tr.insertCell();
       td.innerText = column; // Take string from placeholder variable and append it to <tr> node
     });
   });
 };
+
+//Converts ISO 8061 time to 24-hour hh:mm format
+function convertTime(dateString) {
+  let oldTime = new Date(dateString);
+  let newTime = oldTime.toLocaleTimeString([], {
+    hour12: false,
+    hour: "2-digit",
+    minute: "2-digit",
+  });
+  return newTime;
+}
+
+//Adds a space at every thousandth place in the revenue
+function convertRevenue(revenueString) {
+  let revenue = Array.from(String(revenueString), Number);
+  for (let i = revenue.length - 3; i > 0; i -= 3) {
+    revenue.splice(i, 0, " ");
+  }
+  revenue = revenue.join("");
+  return revenue;
+}
