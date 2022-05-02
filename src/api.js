cd9007b6df49550b02e9938f924d38b98af61798
troy <troywebb95@gmail.com>
2022-05-02 21:21:05 +0100

    reformated table ytd revenue

--- src/api.js
+++ src/api.js
@@ -1,12 +1,22 @@
 import axios from "axios";
-import {BACKEND_BASE_URI, COMPANIES_API_PATH} from "./constants";
+import { BACKEND_BASE_URI, COMPANIES_API_PATH } from "./constants";
 
 /*
   Fetch list of all companies from backend
  */
 export const fetchCompanies = async () => {
   try {
-    const response = await axios.get(`${BACKEND_BASE_URI}${COMPANIES_API_PATH}`);
+    const formatter = new Intl.NumberFormat("en-US", {
+      style: "currency",
+      currency: "USD",
+    });
+    const response = await axios.get(
+      `${BACKEND_BASE_URI}${COMPANIES_API_PATH}`
+    );
+    //for each company, format revenue_ytd field to USD
+    response.data.forEach((company) => {
+      company.revenue_ytd = formatter.format(company.revenue_ytd);
+    });
     return response.data;
   } catch (err) {
     console.log(`Error occurred while fetching companies. Error: ${err}`);
