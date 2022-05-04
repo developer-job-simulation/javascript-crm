698bb21dc2f4c6e3af0ed084226d902df2d54dae
Mindaugas <mindaugas.antanas.grigaitis@gmail.com>
2022-05-04 14:30:36 +0300

    Changed time formatting

--- src/ui.js
+++ src/ui.js
@@ -25,7 +25,7 @@ export const makeTable = async () => {
     row.push(
       company[COMPANY_NAME_FIELD_NAME],
       company[STATUS_FIELD_NAME],
-      company[CREATED_AT_FIELD_NAME],
+      `${String(new Date(company[CREATED_AT_FIELD_NAME]).getUTCHours()).padStart(2, 0)}:${String(new Date(company[CREATED_AT_FIELD_NAME]).getUTCMinutes()).padStart(2, 0)}`,
       company[REVENUE_YTD_FIELD_NAME],
       company[ACCOUNT_EXECUTIVE_FIELD_NAME]
     );
