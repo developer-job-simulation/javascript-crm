eaace17b204130b381567f2b87de0a79c8c826c4
murraydeam <73445952+murraydeam@users.noreply.github.com>
2022-08-20 10:42:22 -0400

    Revenue YTD items have been updated to format with commas.

--- src/api.js
+++ src/api.js
@@ -1,13 +1,17 @@
 import axios from "axios";
-import {BACKEND_BASE_URI, COMPANIES_API_PATH} from "./constants";
+import { BACKEND_BASE_URI, COMPANIES_API_PATH } from "./constants";
 
 /*
   Fetch list of all companies from backend
  */
 export const fetchCompanies = async () => {
   try {
-    const response = await axios.get(`${BACKEND_BASE_URI}${COMPANIES_API_PATH}`);
-    return response.data;
+    const response = await axios.get(
+      `${BACKEND_BASE_URI}${COMPANIES_API_PATH}`
+    );
+    
+    return response.data
+    
   } catch (err) {
     console.log(`Error occurred while fetching companies. Error: ${err}`);
     // Rethrowing error here. If we don't do this, then this function is going to return 'undefined' as opposed to throwing an error
