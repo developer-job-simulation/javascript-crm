1f100462263a1504667f21f3196abbe4cdfcdffc
Juliana Praxedes <julianapraxeds@gmail.com>
2022-11-18 14:18:13 -0300

    Changed 'Created at' column to display 24-hour time format

--- src/ui.js
+++ src/ui.js
@@ -13,7 +13,7 @@ export const makeTable = async () => {
   // Print result of api call to the developer console
   // Uncomment if you need it for debugging.
   // While this method of logging variables of interest to the console is primitive, but often highly valuable debugging technique
-  console.log(companies);
+  // console.log(companies);
 
   // Initialize new array and push a header row
   const companiesToDisplay = [];
@@ -26,10 +26,12 @@ export const makeTable = async () => {
   // Here we simply rearrange company fields in the order in which we want to display them in UI
   companies.map(company => {
     const row = [];
+    const companyCreatedTime = new Date(company[CREATED_AT_FIELD_NAME])
+    // console.log(typeof String(companyCreatedTime))
     row.push(
       company.name,
       company[STATUS_FIELD_NAME],
-      company[CREATED_AT_FIELD_NAME],
+      String(companyCreatedTime).substring(15,21),
       readableNum(company[REVENUE_YTD_FIELD_NAME]),
       company[ACCOUNT_EXECUTIVE_FIELD_NAME]
     );
