36c849b1af316a0d7fc70d7542c6f19a1cd4a4ea
Cesar's PC <cesar261110@gmaill.com>
2022-11-28 15:43:28 -0800

    Implemented formatting iso 8601 to display only time created at

--- src/ui.js
+++ src/ui.js
@@ -20,13 +20,14 @@ export const makeTable = async () => {
   const companiesToDisplay = [];
   companiesToDisplay.push(COMPANIES_TABLE_HEADERS);
 
+
   // Here we simply rearrange company fields in the order in which we want to display them in UI
   companies.map(company => {
     const row = [];
     row.push(
       company[COMPANY_NAME_FIELD_NAME],
       company[STATUS_FIELD_NAME],
-      company[CREATED_AT_FIELD_NAME],
+      company[CREATED_AT_FIELD_NAME].substr(11, 5),
       company[REVENUE_YTD_FIELD_NAME].toString().replace(/\B(?=(\d{3})+(?!\d))/g, " "),
       company[ACCOUNT_EXECUTIVE_FIELD_NAME]
     );
