a69583ffe14c88d59c047664eecfb8eef779c23e
sean <custodiocsean@gmail.com>
2023-02-23 12:13:47 +0800

    4 issues fix 1st attempt

--- src/ui.js
+++ src/ui.js
@@ -1,11 +1,11 @@
-import {fetchCompanies} from "./api";
+import { fetchCompanies } from "./api";
 import {
   ACCOUNT_EXECUTIVE_FIELD_NAME,
   COMPANIES_TABLE_HEADERS,
   COMPANY_NAME_FIELD_NAME,
   CREATED_AT_FIELD_NAME,
   REVENUE_YTD_FIELD_NAME,
-  STATUS_FIELD_NAME
+  STATUS_FIELD_NAME,
 } from "./constants";
 
 export const makeTable = async () => {
@@ -13,20 +13,21 @@ export const makeTable = async () => {
   // Print result of api call to the developer console
   // Uncomment if you need it for debugging.
   // While this method of logging variables of interest to the console is primitive, but often highly valuable debugging technique
-  // console.log(companies);
 
   // Initialize new array and push a header row
   const companiesToDisplay = [];
   companiesToDisplay.push(COMPANIES_TABLE_HEADERS);
 
   // Here we simply rearrange company fields in the order in which we want to display them in UI
-  companies.map(company => {
+  companies.map((company) => {
+    const created_at = new Date(Date.parse(company[CREATED_AT_FIELD_NAME]));
+    console.log();
     const row = [];
     row.push(
-      company[COMPANY_NAME_FIELD_NAME],
+      company.name,
       company[STATUS_FIELD_NAME],
-      company[CREATED_AT_FIELD_NAME],
-      company[REVENUE_YTD_FIELD_NAME],
+      `${created_at.getUTCHours()}:${created_at.getUTCMinutes()}`,
+      company[REVENUE_YTD_FIELD_NAME].toLocaleString().replaceAll(",", " "),
       company[ACCOUNT_EXECUTIVE_FIELD_NAME]
     );
     companiesToDisplay.push(row);
@@ -36,10 +37,10 @@ export const makeTable = async () => {
   const table = document.createElement("table");
   document.body.appendChild(table); // Drew the main table node on the document
 
-  companiesToDisplay.forEach(row => {
+  companiesToDisplay.forEach((row) => {
     const tr = table.insertRow(); //Create a new row
 
-    row.forEach(column => {
+    row.forEach((column) => {
       const td = tr.insertCell();
       td.innerText = column; // Take string from placeholder variable and append it to <tr> node
     });
