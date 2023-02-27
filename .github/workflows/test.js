e904c5b8b98eebd55413e97c29668e5d3c80b9eb
sbmsr <sebastian.messier@gmail.com>
2023-02-27 14:03:41 -0500

    playwright

--- .github/workflows/test.js
+++ .github/workflows/test.js
@@ -1,100 +1,91 @@
+import { chromium } from "playwright";
 import { setTimeout } from "timers/promises";
 import { test } from "uvu";
 import * as assert from "uvu/assert";
-import * as Puppeteer from "./setup/puppeteer.js";
 
-test.before(async (context) => {
-  await Puppeteer.setup(context);
+let browser;
+let context;
+let page;
+
+test.before(async () => {
+  browser = await chromium.launch({
+    use: { timezoneId: "Etc/UTC" },
+  });
+  context = await browser.newContext();
 });
 
-test.after(async (context) => {
-  await Puppeteer.close(context);
+test.before.each(async () => {
+  page = await context.newPage();
 });
 
-test("Solved Issue #1: Company Names are Present", async (context) => {
-  await Puppeteer.homepage(context);
+test.after.each(async () => {
+  await page.close();
+});
+
+test.after(async () => {
+  await browser.close();
+  await context.close();
+});
+
+test("Solved Issue #1: Company Names are Present", async () => {
+  await page.goto("http://localhost:8080");
   await setTimeout(250);
 
-  var text = await context.page.evaluate(() => {
-    return document.querySelector("body > table > tbody > tr:nth-child(2) > td:nth-child(1)").textContent;
-  });
+  var text = await page.$eval("body > table > tbody > tr:nth-child(2) > td:nth-child(1)", (el) => el.textContent);
   assert.type(text, "string");
   assert.is(text, "Fusion LLC");
 
-  var text = await context.page.evaluate(() => {
-    return document.querySelector("body > table > tbody > tr:nth-child(3) > td:nth-child(1)").textContent;
-  });
+  var text = await page.$eval("body > table > tbody > tr:nth-child(3) > td:nth-child(1)", (el) => el.textContent);
   assert.type(text, "string");
   assert.is(text, "Techopolis Ltd.");
 
-  var text = await context.page.evaluate(() => {
-    return document.querySelector("body > table > tbody > tr:nth-child(4) > td:nth-child(1)").textContent;
-  });
+  var text = await page.$eval("body > table > tbody > tr:nth-child(4) > td:nth-child(1)", (el) => el.textContent);
   assert.type(text, "string");
   assert.is(text, "Code learning LLC");
 });
 
-test("Solved Issue #2: display dates in 24-hour time format", async (context) => {
-  await Puppeteer.homepage(context);
+test("Solved Issue #2: display dates in 24-hour time format", async () => {
+  await page.goto("http://localhost:8080");
   await setTimeout(250);
 
-  var text = await context.page.evaluate(() => {
-    return document.querySelector("body > table > tbody > tr:nth-child(2) > td:nth-child(3)").textContent;
-  });
+  var text = await page.$eval("body > table > tbody > tr:nth-child(2) > td:nth-child(3)", (el) => el.textContent);
   assert.type(text, "string");
   assert.is(text, "03:41");
 
-  var text = await context.page.evaluate(() => {
-    return document.querySelector("body > table > tbody > tr:nth-child(3) > td:nth-child(3)").textContent;
-  });
+  var text = await page.$eval("body > table > tbody > tr:nth-child(3) > td:nth-child(3)", (el) => el.textContent);
   assert.type(text, "string");
   assert.is(text, "08:45");
 
-  var text = await context.page.evaluate(() => {
-    return document.querySelector("body > table > tbody > tr:nth-child(4) > td:nth-child(3)").textContent;
-  });
+  var text = await page.$eval("body > table > tbody > tr:nth-child(4) > td:nth-child(3)", (el) => el.textContent);
   assert.type(text, "string");
   assert.is(text, "12:45");
 });
 
-test("Solved Issue #3: display revenue numbers in a human readable format", async (context) => {
-  await Puppeteer.homepage(context);
+test("Solved Issue #3: display revenue numbers in a human readable format", async () => {
+  await page.goto("http://localhost:8080");
   await setTimeout(250);
 
-  var text = await context.page.evaluate(() => {
-    return document.querySelector("body > table > tbody > tr:nth-child(2) > td:nth-child(4)").textContent;
-  });
+  var text = await page.$eval("body > table > tbody > tr:nth-child(2) > td:nth-child(4)", (el) => el.textContent);
   assert.type(text, "string");
   assert.is(text, "17 000 000");
 
-  var text = await context.page.evaluate(() => {
-    return document.querySelector("body > table > tbody > tr:nth-child(3) > td:nth-child(4)").textContent;
-  });
+  var text = await page.$eval("body > table > tbody > tr:nth-child(3) > td:nth-child(4)", (el) => el.textContent);
   assert.type(text, "string");
   assert.is(text, "7 375 294");
 
-  var text = await context.page.evaluate(() => {
-    return document.querySelector("body > table > tbody > tr:nth-child(4) > td:nth-child(4)").textContent;
-  });
+  var text = await page.$eval("body > table > tbody > tr:nth-child(4) > td:nth-child(4)", (el) => el.textContent);
   assert.type(text, "string");
   assert.is(text, "100 000");
 });
 
-test("Solved Issue #4: make table look prettier", async (context) => {
-  await Puppeteer.homepage(context);
+test("Solved Issue #4: make table look prettier", async () => {
+  await page.goto("http://localhost:8080");
   await setTimeout(250);
 
-  var headerColor = await context.page.evaluate(() => {
-    return getComputedStyle(document.getElementsByTagName("tr")[0])["background-color"];
-  });
-
+  var headerColor = await page.$eval("tr:first-of-type", (el) =>
+    getComputedStyle(el).getPropertyValue("background-color")
+  );
   assert.is(headerColor, "rgb(173, 216, 230)");
-
-  // var tableBorderColor = await context.page.evaluate(() => {
-  //   let table = document.querySelector("body > table");
-  //   return window.getComputedStyle(table).getPropertyValue("border-color");
-  // });
-  // assert.is(tableBorderColor, "rgb(173, 216, 230)");
 });
 
 test.run();
