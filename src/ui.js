cfc51ae5382a90b7a007e24d0abde5e82bab0786
kelly <kelly.tate.dev@gmail.com>
2023-04-29 19:00:20 -0700

    Formats 'CREATED_AT' field.

--- src/ui.js
+++ src/ui.js
@@ -8,6 +8,10 @@ import {
   STATUS_FIELD_NAME
 } from "./constants";
 
+function format2Digits(num) {
+  return num.toString().padStart(2, '0');
+}
+
 export const makeTable = async () => {
   const companies = await fetchCompanies();
   // Print result of api call to the developer console
@@ -15,17 +19,28 @@ export const makeTable = async () => {
   // While this method of logging variables of interest to the console is primitive, but often highly valuable debugging technique
   // console.log(companies);
 
+
+
   // Initialize new array and push a header row
   const companiesToDisplay = [];
   companiesToDisplay.push(COMPANIES_TABLE_HEADERS);
 
   // Here we simply rearrange company fields in the order in which we want to display them in UI
   companies.map(company => {
+
+    // Convert date format from ISO 8601 to hh:mm
+    // const date = new Date((company[CREATED_AT_FIELD_NAME]).slice(0, -1));
+    // const time = new Date(date.getTime());
+    // const hours = time.getHours();
+    // const minutes = time.getMinutes();
+    // const convertedTime = `${format2Digits(hours)}:${format2Digits(minutes)}`;
+    const formattedTime = (company[CREATED_AT_FIELD_NAME]).slice(11, 16);
+
     const row = [];
     row.push(
       company[COMPANY_NAME_FIELD_NAME],
       company[STATUS_FIELD_NAME],
-      company[CREATED_AT_FIELD_NAME],
+      formattedTime,
       company[REVENUE_YTD_FIELD_NAME],
       company[ACCOUNT_EXECUTIVE_FIELD_NAME]
     );
