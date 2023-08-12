578f153c25bb8d3b0a7ce30cadb6bb541ecb6510
Anton Gundermann <anton.gundermann@gmx.de>
2023-08-12 13:11:14 +0200

    fixed: all issues

--- src/ui.js
+++ src/ui.js
@@ -10,13 +10,10 @@ import {
 
 const formatDate = (str) => {
   const date = new Date(str)
-  const year = date.getFullYear()
-  const month = String(date.getMonth() + 1).padStart(2, '0')
-  const day = String(date.getDate()).padStart(2, '0')
   const hours = String(date.getHours()).padStart(2, '0')
   const minutes = String(date.getMinutes()).padStart(2, '0')
 
-  return `${year}-${month}-${day} ${hours}:${minutes}`
+  return `${hours}:${minutes}`
 }
 
 const formatNumber = (num) => {
