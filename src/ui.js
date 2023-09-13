3e21c75e52260fe79108562403c0cdc8e5cad49e
MarcusKyung <Kyungmj@gmail.com>
2023-09-12 22:56:01 -0700

    add: space to revenue for readability

--- src/ui.js
+++ src/ui.js
@@ -19,6 +19,11 @@ export const makeTable = async () => {
   const companiesToDisplay = [];
   companiesToDisplay.push(COMPANIES_TABLE_HEADERS);
 
+  function addSpace(revenue) {
+    let formattedNumber = revenue.toLocaleString().replace(/,/g, ' ');
+    return formattedNumber;
+  };
+
   // Here we simply rearrange company fields in the order in which we want to display them in UI
   companies.map(company => {
     const row = [];
@@ -26,7 +31,7 @@ export const makeTable = async () => {
       company[COMPANY_NAME_FIELD_NAME],
       company[STATUS_FIELD_NAME],
       company[CREATED_AT_FIELD_NAME],
-      company[REVENUE_YTD_FIELD_NAME],
+      addSpace(company[REVENUE_YTD_FIELD_NAME]),
       company[ACCOUNT_EXECUTIVE_FIELD_NAME]
     );
     companiesToDisplay.push(row);
