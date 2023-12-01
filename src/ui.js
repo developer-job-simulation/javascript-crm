d6fcf193f44fc2d2b9f3634b08471f498a1543ef
Anne Chaiyato <a.chaiyato@gmail.com>
2023-12-01 09:26:41 -0800

    Fixes javascript-crm Issues #1 - #4

--- src/ui.js
+++ src/ui.js
@@ -13,7 +13,7 @@ export const makeTable = async () => {
   // Print result of api call to the developer console
   // Uncomment if you need it for debugging.
   // While this method of logging variables of interest to the console is primitive, but often highly valuable debugging technique
-  // console.log(companies);
+  console.log("Companies are ",companies);
 
   // Initialize new array and push a header row
   const companiesToDisplay = [];
@@ -21,12 +21,15 @@ export const makeTable = async () => {
 
   // Here we simply rearrange company fields in the order in which we want to display them in UI
   companies.map(company => {
+    const created_time = company[CREATED_AT_FIELD_NAME];
+    const reformatted_time = new Date(created_time).toLocaleTimeString('en', {timeStyle: 'short', hour12: false, timeZone: 'UTC'});
+
     const row = [];
     row.push(
       company[COMPANY_NAME_FIELD_NAME],
       company[STATUS_FIELD_NAME],
-      company[CREATED_AT_FIELD_NAME],
-      company[REVENUE_YTD_FIELD_NAME],
+      reformatted_time,
+      company[REVENUE_YTD_FIELD_NAME].toLocaleString().split(',').join(' '),
       company[ACCOUNT_EXECUTIVE_FIELD_NAME]
     );
     companiesToDisplay.push(row);
