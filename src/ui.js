5ab4e61e342f53e1e0ed68c97e52b869def095e1
Lucky Khoza <84252978+LuckyKhoza-crypto@users.noreply.github.com>
2025-02-11 15:03:55 -0800

    fixed all listed issues

--- src/ui.js
+++ src/ui.js
@@ -1,11 +1,11 @@
-import {fetchCompanies} from "./api";
+import { fetchCompanies } from "./api";
 import {
   ACCOUNT_EXECUTIVE_FIELD_NAME,
   COMPANIES_TABLE_HEADERS,
   COMPANY_NAME_FIELD_NAME,
   CREATED_AT_FIELD_NAME,
   REVENUE_YTD_FIELD_NAME,
-  STATUS_FIELD_NAME
+  STATUS_FIELD_NAME,
 } from "./constants";
 
 export const makeTable = async () => {
@@ -20,13 +20,13 @@ export const makeTable = async () => {
   companiesToDisplay.push(COMPANIES_TABLE_HEADERS);
 
   // Here we simply rearrange company fields in the order in which we want to display them in UI
-  companies.map(company => {
+  companies.map((company) => {
     const row = [];
     row.push(
       company[COMPANY_NAME_FIELD_NAME],
       company[STATUS_FIELD_NAME],
-      company[CREATED_AT_FIELD_NAME],
-      company[REVENUE_YTD_FIELD_NAME],
+      Date(company[CREATED_AT_FIELD_NAME]),
+      Intl.NumberFormat("fr-FR").format(company[REVENUE_YTD_FIELD_NAME]),
       company[ACCOUNT_EXECUTIVE_FIELD_NAME]
     );
     companiesToDisplay.push(row);
@@ -36,10 +36,10 @@ export const makeTable = async () => {
   const table = document.createElement("table");
   document.body.appendChild(table); // Drew the main table node on the document
 
-  companiesToDisplay.forEach(row => {
+  companiesToDisplay.forEach((row) => {
     const tr = table.insertRow(); //Create a new row
 
-    row.forEach(column => {
+    row.forEach((column) => {
       const td = tr.insertCell();
       td.innerText = column; // Take string from placeholder variable and append it to <tr> node
     });
